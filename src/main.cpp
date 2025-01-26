4ba90382bc6a985befce84c16bbd36781cd48ba4
vitalii_zhuk <vitalii.zhuk@masterofcode.com>
2025-01-26 14:51:11 +0200

    mag data using QMC5883

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 #include <string>
 #include <iostream>
 #include <sstream>
-#include <DFRobot_BMM150.h>
+#include <DFRobot_QMC5883.h>
 #define SCK     5    // GPIO5  -- SCK
 #define MISO    19   // GPIO19 -- MISO
 #define MOSI    27   // GPIO27 -- MOSI
@@ -18,7 +18,7 @@
 #define BAND    433E6
 const String deviceKey = "killRusnya_2";
 
-const float minusValueForAzimuth = 30.3;
+const float minusValueForAzimuth = 0;
 const float plusValueForAzimuth = 0;
 
 const int module1OutputPin = 4;
@@ -46,9 +46,9 @@ float azimut;
 int counter = 0;
 int lastAzimutCheckin = 0;
 
-int initMagDataX = 0;
-int initMagDataY = 0;
-int initMagDataZ = 0;
+double initMagDataX = 0;
+double initMagDataY = 0;
+double initMagDataZ = 0;
 
 // for LED 
 #define OLED_I2C_ADDRESS 0x3C
@@ -60,20 +60,55 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #include <Adafruit_Sensor.h>
 #include <Adafruit_HMC5883_U.h>
 
+/**
+ * @brief Creates an instance of the DFRobot_QMC5883 class to interface with the QMC5883L magnetometer sensor.
+ * 
+ * This object is used to communicate with the QMC5883L sensor over the I2C bus using the specified address.
+ * 
+ * @param Wire The I2C bus interface.
+ * @param QMC5883L_ADDRESS The I2C address of the QMC5883L sensor.
+ */
+DFRobot_QMC5883 compass(&Wire, 0x1E);
 
-DFRobot_BMM150_I2C bmm150(&Wire, I2C_ADDRESS_4);
+// DFRobot_BMM150_I2C bmm150(&Wire, I2C_ADDRESS_4);
 
-void setInitMagData(){
-  sBmm150MagData_t magData = bmm150.getGeomagneticData();
-  initMagDataX = magData.x;
-  initMagDataY = magData.y;
-  initMagDataZ = magData.z;  
-}
+// void setInitMagData(){
+//   sBmm150MagData_t magData = bmm150.getGeomagneticData();
+//   initMagDataX = magData.x;
+//   initMagDataY = magData.y;
+//   initMagDataZ = magData.z;  
+// }
 
 void setup() {
   Serial.begin(9600);
   while (!Serial);
   Serial.println("LoRa Receiver");
+
+ // Initialize I2C with specific SCL and SDA pins
+  Wire.begin(21, 22);
+
+  // I2C Scanner
+  Serial.println("Scanning for I2C devices...");
+  byte error, address;
+  int nDevices = 0;
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16) Serial.print("0");
+      Serial.print(address, HEX);
+      Serial.println(" !");
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16) Serial.print("0");
+      Serial.println(address, HEX);
+    }
+  }
+  if (nDevices == 0) Serial.println("No I2C devices found\n");
+  else Serial.println("done\n");
+
   SPI.begin();
   LoRa.setPins(SS,RST,DI0);
 
@@ -91,21 +126,35 @@ void setup() {
   }
   display.display();
   delay(2000); // Pause for 2 seconds
-  while(bmm150.begin()){
-    Serial.println("bmm150 init failed, Please try again!");
-    delay(1000);
-  } Serial.println("bmm150 init success!");
-  bmm150.setOperationMode(BMM150_POWERMODE_NORMAL);
-  bmm150.setPresetMode(BMM150_PRESETMODE_HIGHACCURACY);
-  bmm150.setRate(BMM150_DATA_RATE_10HZ);
+  // while(bmm150.begin()){
+  //   Serial.println("bmm150 init failed, Please try again!");
+  //   delay(1000);
+  // } Serial.println("bmm150 init success!");
+  // bmm150.setOperationMode(BMM150_POWERMODE_NORMAL);
+  // bmm150.setPresetMode(BMM150_PRESETMODE_HIGHACCURACY);
+  // bmm150.setRate(BMM150_DATA_RATE_10HZ);
 
   /**!
    * Enable the measurement at x-axis, y-axis and z-axis, default to be enabled, no config required, the geomagnetic data at x, y and z will be incorrect when disabled.
    * Refer to setMeasurementXYZ() function in the .h file if you want to configure more parameters.
    */
-  bmm150.setMeasurementXYZ();
-  delay(1000);
-  setInitMagData();
+  // bmm150.setMeasurementXYZ();
+  // delay(1000);
+  // setInitMagData();
+
+  // Initialize the compass
+  if (!compass.begin()) {
+    Serial.println("Could not find a valid QMC5883 sensor, check wiring!");
+    while (1);
+  }
+  sVector_t mag = compass.readRaw();
+  initMagDataX = mag.XAxis;
+  initMagDataY = mag.YAxis;
+  initMagDataZ = mag.ZAxis; // Corrected to use ZAxis
+  // compass.setRange(QMC5883_RANGE_8GA);
+  // compass.setMeasurementMode(QMC5883_CONTINOUS);
+  // compass.setDataRate(QMC5883_DATARATE_200HZ);
+  // compass.setSamples(QMC5883_SAMPLES_2);
 }
 void printInfo(){
   display.clearDisplay();
@@ -159,40 +208,44 @@ void sendMagDataChanges() {
   lastOutputCommand = magDataChangedCommand;
 }
  void checkAzimut(){
-  sBmm150MagData_t magData = bmm150.getGeomagneticData();
+  float declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / PI);
+  compass.setDeclinationAngle(declinationAngle);
+  sVector_t mag = compass.readRaw();
+  double x = mag.XAxis;
+  double y = mag.YAxis;
+  double z = mag.ZAxis; // Corrected to use ZAxis
+  azimut = mag.HeadingDegress; // Corrected to use getHeadingDegrees method
+  Serial.print("mag x = "); Serial.print(x); Serial.println(" uT");
+  Serial.print("mag y = "); Serial.print(y); Serial.println(" uT");
+  Serial.print("mag z = "); Serial.print(z); Serial.println(" uT");
+
+  // azimuth = azimuth - minusValueForAzimuth + plusValueForAzimuth;
   
-  double magDataX = magData.x;
-  double magDataY = magData.y;
-  double magDataZ = magData.z;
-  Serial.print("mag x = "); Serial.print(magDataX); Serial.println(" uT");
-  Serial.print("mag y = "); Serial.print(magDataY); Serial.println(" uT");
-  Serial.print("mag z = "); Serial.print(magDataZ); Serial.println(" uT");
+  // Serial.print("the angle between the pointing direction and north (counterclockwise) is:");
+  // Serial.println(azimut);
+  // Serial.println("--------------------------------");
 
-  azimut = bmm150.getCompassDegree();
-  Serial.print("the angle between the pointing direction and north (counterclockwise) is:");
-  Serial.println(azimut);
-  Serial.println("--------------------------------");
-
-  azimut = azimut - minusValueForAzimuth + plusValueForAzimuth;
-  Serial.print("corrected azimut is:");
-  Serial.println(azimut);
-  Serial.println("--------------------------------");
+  // azimut = azimut - minusValueForAzimuth + plusValueForAzimuth;
+  // Serial.print("corrected azimut is:");
+  // Serial.println(azimut);
+  // Serial.println("--------------------------------");
   delay(100);
  }
 
 void checkMagData(){
-  sBmm150MagData_t magData = bmm150.getGeomagneticData();
-  int magDataX = magData.x;
-  int magDataY = magData.y;
-  int magDataZ = magData.z;
-  if (abs(magDataX - initMagDataX) > 0 || abs(magDataY - initMagDataY) > 0 || abs(magDataZ - initMagDataZ) > 0) {
+  // float declinationAngle = (4.0 + (26.0 / 60.0)) / (180 / PI);
+  // compass.setDeclinationAngle(declinationAngle);
+  sVector_t mag = compass.readRaw();
+  double x = mag.XAxis;
+  double y = mag.YAxis;
+  double z = mag.ZAxis; // Corrected to use ZAxis
+  if (abs(x - initMagDataX) > 3 || abs(y - initMagDataY) > 3 || abs(z - initMagDataZ) > 3) {
     Serial.println("Magnetic data has changed significantly.");
     sendMagDataChanges();
-    initMagDataX = magDataX;
-    initMagDataY = magDataY;
-    initMagDataZ = magDataZ;
+    initMagDataX = x;
+    initMagDataY = y;
+    initMagDataZ = z;
   }
-
 }
 
 
@@ -265,4 +318,5 @@ void loop() {
     sendAzimutCommand(azimut);
     lastAzimutCheckin = counter;
   }
+  
 }
